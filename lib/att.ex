08540cf0263e453f9ec5c94780b3a7e8c74e5c59
alexis <sawcce@proton.me>
2023-12-10 15:40:45 +0100

    Base work for BucketManager

--- lib/att.ex
+++ lib/att.ex
@@ -1,14 +1,20 @@
 defmodule Att do
+  require Logger
+
   def start(_type, _args) do
     {:ok, pid} = DynamicSupervisor.start_link(strategy: :one_for_one)
 
     webserver = {Plug.Cowboy, plug: WebInterface, scheme: :http, options: [port: 4000]}
 
+    DynamicSupervisor.start_child(pid, {BucketManager, pid})
     DynamicSupervisor.start_child(pid, webserver)
-    DynamicSupervisor.start_child(pid, {Store, "main"})
 
-    IO.puts "Workers:"
-    IO.inspect Supervisor.which_children(pid)
+    IO.puts "Supervised instances:"
+
+    Enum.each Supervisor.which_children(pid), fn(item) ->
+        {_, pid, role, instance} = item
+        Logger.info("[Supervised] Role: #{role}, PID: #{inspect(pid)}, #{inspect(instance)}")
+    end
 
     Process.sleep(:infinity)
 
