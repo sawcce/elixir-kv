b28abe187f9e8c2e7cc99f46e7e2c6f9cb2d0464
alexis <sawcce@proton.me>
2023-12-17 14:07:19 +0100

    Made server listen on public ip

--- lib/att.ex
+++ lib/att.ex
@@ -4,7 +4,7 @@ defmodule Att do
   def start(_type, _args) do
     {:ok, pid} = DynamicSupervisor.start_link(strategy: :one_for_one)
 
-    webserver = {Plug.Cowboy, plug: WebInterface, scheme: :http, options: [port: 4000]}
+    webserver = {Plug.Cowboy, plug: WebInterface, scheme: :http, options: [ip: {0, 0, 0, 0}, port: 4000]}
 
     DynamicSupervisor.start_child(pid, {BucketManager, pid})
     DynamicSupervisor.start_child(pid, webserver)
